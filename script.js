a8bc5b12f983428dd212b09596bf9cae44948260
onlinemath85@gmail.com <onlinemath85@gmail.com>
2024-05-02 03:27:28 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+//  naviagation bar function
+
+function MyMenuFunction(){
+    var menuBtn = document.getElementById("MyNavMenu");
+    if(menuBtn.className === "nav-menu"){
+        menuBtn.className += "responsive";
+    }else{
+        menuBtn.className = "nav-menu"
+    }
+}
+
+// type effect
+
+var typingEffect = new Typed(".typedText",{
+    strings:[ "Designer","Web_Devloper","Freelancer"],
+    loop:true,  
+    typeSpeed:100,
+     backSpeed:80,
+    backDelay:2000
+
+})
+
+// scroller revel animation
+
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'80px',
+    duration:2000,
+    reset:true
+})
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay:100})
+sr.reveal('.featured-text-info',{delay:200})
+sr.reveal('.featured-text-btn',{delay:200})
+sr.reveal('.social-icons',{delay:200})
+sr.reveal('.col-4',{delay:300})
+sr.reveal('.col-8',{delay:300})
+sr.reveal('.col',{delay:300})
+
+sr.reveal('project-box',{interval:200})
+sr.reveal('portfolio-box',{interval:200})
+sr.reveal('.top-header',{})
+const srLeft = ScrollReveal({
+    origin:'left',
+    distance:'80px',
+    duration:2000,
+    reset:true
+})
+sr.srLeft.reveal('.contact-info',{delay:100})
+
+const srRight = ScrollReveal({
+    origin:'right',
+    distance:'80px',
+    duration:2000,
+    reset:true
+})
+srRight.reveal('.skills-box',{delay:100})
+srRight.reveal('.form-control',{delay:100})
+
+// change active link
+
+const sections=document.querySelectorAll('section[id]')
+function scrollActive(){
+    const scrollY= window.scrollY;
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 50,
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[herf*-' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav-menu a[herf*-' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll',scrollActive)
